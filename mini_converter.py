411c9e23dc2e9d052a4ed527cab4c3260c7a0b14
andreveeguil <andreveeguil@gmail.com>
2022-10-03 20:53:37 +0800

    master): fix some bugs

--- mini_converter.py
+++ mini_converter.py
@@ -1,7 +1,25 @@
 import os
+import argparse
 
-root_folder = "C:/ASTrio/VS2017/Desktop/Sipebi/Sipebi.Core"
-dest_folder = "C:/ASTrio/VS2017/Desktop/Sipebi"
+parser = argparse.ArgumentParser(
+    description="Insert the correct source and destination folders"
+)
+parser.add_argument(
+    "--source_folder",
+    type=str,
+    default="C:/ASTrio/VS2017/Desktop/Sipebi/Sipebi.Core",
+    help="Source folder for Sipebi",
+)
+parser.add_argument(
+    "--dest_folder",
+    type=str,
+    default="C:/ASTrio/VS2017/Desktop/Sipebi",
+    help="Destination folder for Sipebi Mini",
+)
+args = parser.parse_args()
+
+source_folder = args.source_folder
+dest_folder = args.dest_folder
 
 rel_file_mapping = {
     # Bug?? Completely deleted??
@@ -20,7 +38,7 @@ rel_file_mapping = {
     "/DataModels/": "SipebiMini.Analyser",
 }
 
-file_mapping = {root_folder + k: v for (k, v) in rel_file_mapping.items()}
+file_mapping = {source_folder + k: v for (k, v) in rel_file_mapping.items()}
 
 rel_namespace_files_list = [
     "/Mini/SipebiMiniEditor.cs",
@@ -29,11 +47,13 @@ rel_namespace_files_list = [
     "/Core/SipebiSettings.cs",
     "/Core/SipebiWordDivision.cs",
     "/Constants/GlobalHolder.cs",
-    "/Mini/SipebiMiniAnalyzer.cs",
+    "/Mini/SipebiMiniAnalyser.cs",
     "/DataModels/Diagnostics/SipebiDiagnosticsError.cs",
+    "/DataModels/Diagnostics/SipebiParagraphDiagnosticsReport.cs",
+    "/DataModels/Diagnostics/SipebiDiagnosticsReport.cs",
 ]
 
-namespace_files_list = [root_folder + f for f in rel_namespace_files_list]
+namespace_files_list = [source_folder + f for f in rel_namespace_files_list]
 
 rel_excluded_files = [
     "/Core/SipebiAnalyser.cs",
@@ -53,7 +73,7 @@ rel_excluded_files = [
     "/Extension/MiscExtension.cs",
 ]
 
-excluded_files = [root_folder + f for f in rel_excluded_files]
+excluded_files = [source_folder + f for f in rel_excluded_files]
 
 
 def recurse_files(source):
@@ -65,7 +85,7 @@ def recurse_files(source):
             nested_files = recurse_files(full_file_path)
             results.extend(nested_files)
         else:
-            results.append(full_file_path.strip("/"))
+            results.append(full_file_path.rstrip("/"))
 
     return results
 
@@ -76,9 +96,11 @@ def recurse_files(source):
 
 # Source can be a single file, or if it is a folder, it will be
 # recursively searched nestedly
-# Looks OK. But not  sure if a folder should always be recursively searched
+# Looks OK. But not sure if a folder should always be recursively searched
+list_last_files = []
 for source, destination in file_mapping.items():
     last_files = recurse_files(source) if source.endswith("/") else [source]
+    list_last_files.extend(last_files)
 
     for last_source in last_files:
         source_f = open(last_source)
@@ -92,7 +114,7 @@ for source, destination in file_mapping.items():
 
         if (
             destination == "SipebiMini.Analyser"
-            and source != root_folder + "/Mini/SipebiMiniAnalyser.cs"
+            and source != source_folder + "/Mini/SipebiMiniAnalyser.cs"
         ):
             if last_source in excluded_files:
                 continue
@@ -113,7 +135,7 @@ for source, destination in file_mapping.items():
             # the way to do it, is by taking the relative path and then adds
             # with whatever addition elements which exist after
             # the relative path
-            last_file_name = last_source[len(root_folder) : len(last_source)]  # noqa
+            last_file_name = last_source[len(source_folder) : len(last_source)]  # noqa
         else:
             last_file_name = last_source.split("/")[-1]
 
@@ -135,13 +157,19 @@ for source, destination in file_mapping.items():
             destination_f.write("using SipebiMini.Core;\n")
 
         for line in code:
+            line = line.replace("\ufeff", "")
             if line.strip().startswith("namespace"):
-                namespace = file_mapping.get(source).split("/")[0]
+                namespace = destination.split("/")[0]
                 # supposedly printing 'namespace SipebiMini.Core {',
                 # but it maybe mistaken to '...Sipebi.Core {'
                 destination_f.write(f"namespace {namespace} \u007b\n")
             # comment removal, only at the beginning of a line though
+            elif "SipebiSettings.cs" in last_file_name and "PH" in line:
+                line = line.replace("PH", "MDH")
+                line = line.replace("SipebiReP", "SipebiMiniP")
+                line = line.replace("SipebiReK", "SipebiMiniK")
+                destination_f.write(line)
             elif line.strip().startswith("//"):
                 continue
             else:
-                destination_f.write(line.replace("\ufeff", ""))
+                destination_f.write(line)
